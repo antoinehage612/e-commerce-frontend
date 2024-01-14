b54542ac3d49015e3acd5e6a0b332b93b8691024
antoinehage612 <hypercoffin612@gmail.com>
2024-01-14 14:16:48 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import Product from "./pages/Product";
+import Register from "./pages/Register";
+import ProductList from "./pages/ProductList";
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Success from "./pages/Success";
+import { useSelector } from "react-redux";
+
+const App = () => {
+  const user = useSelector((state) => state.user.currentUser);
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/success" element={<Success />} />
+        <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+        <Route
+          path="/register"
+          element={user ? <Navigate to="/" /> : <Register />}
+        />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/products/:category" element={<ProductList />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/requestMethods.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:5000/api/";
+const TOKEN =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1OWQzNGEzZjhjZjE1OGU3OTc2ZmUxYSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTcwNDg4NTY5NCwiZXhwIjoxNzA1MTQ0ODk0fQ.ZzaMKdHLxCdBBK5SyXdrzxzQ7yCFkH4ncoKfCPAWdGg";
+
+export const publicRequest = axios.create({
+  baseURL: BASE_URL,
+});
+
+export const userRequest = axios.create({
+  baseURL: BASE_URL,
+  header: { token: `Bearer ${TOKEN}` },
+});
